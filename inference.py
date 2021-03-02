442a624bb70facdf0016a60aea1e61997755a731
Rishikesh (ऋषिकेश) <rishikksh20@gmail.com>
2021-03-02 15:20:22 +0530

    :books: Update inference code

--- inference.py
+++ inference.py
@@ -43,7 +43,7 @@ def inference(a):
     filelist = os.listdir(a.input_wavs_dir)
 
     os.makedirs(a.output_dir, exist_ok=True)
-
+    pqmf = PQMF(N=4, taps=62, cutoff=0.15, beta=9.0).cuda()
     generator.eval()
     generator.remove_weight_norm()
     with torch.no_grad():
@@ -53,6 +53,9 @@ def inference(a):
             wav = torch.FloatTensor(wav).to(device)
             x = get_mel(wav.unsqueeze(0))
             y_g_hat = generator(x)
+            if h.output_channel > 1:
+                y_mb_ = y_g_hat
+                y_g_hat = pqmf.synthesis(y_mb_)
             audio = y_g_hat.squeeze()
             audio = audio * MAX_WAV_VALUE
             audio = audio.cpu().numpy().astype('int16')
@@ -60,7 +63,6 @@ def inference(a):
             output_file = os.path.join(a.output_dir, os.path.splitext(filname)[0] + '_generated.wav')
             write(output_file, h.sampling_rate, audio)
             print(output_file)
-
 
 def main():
     print('Initializing Inference Process..')
